694525a96f60e05f72acdaf2d3ad5d7bdd24b372
Raphael <>
2021-05-18 12:36:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let Discord = require("discord.js")
+let client = new Discord.Client()
+const config = require('./config.json')
+const command = require('./commands.js')
+const express = require("express")
+const app = express()
+const mySecret = process.env['token']
+
+app.get("/", (req, res) => {
+  res.send("Holaaa que onda pa, ya estoy en linea Less Gouuu")
+})
+
+app.listen(3000, () => {
+  console.log("Ya tamo listo pa, prendido y andando yeahhh")
+})
+
+client.on('ready', () => {
+	client.user.setActivity(`Eso`, {type: "WATCHING"})
+});
